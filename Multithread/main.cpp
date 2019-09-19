cb86f554d4b830247cd33a7df8285b0e7dbdc296
贾晓凯 <jiaxiaokai@techbloom.net>
2019-09-19 18:41:23 +0800

    Add MulitiThread

---
+++ Multithread/main.cpp
@@ -0,0 +1,71 @@
+﻿/*
+我们提供了一个类：
+
+public class Foo {
+	public void one() { print("one"); }
+	public void two() { print("two"); }
+	public void three() { print("three"); }
+}
+三个不同的线程将会共用一个 Foo 实例。
+
+线程 A 将会调用 one() 方法
+线程 B 将会调用 two() 方法
+线程 C 将会调用 three() 方法
+请设计修改程序，以确保 two() 方法在 one() 方法之后被执行，three() 方法在 two() 方法之后被执行。
+
+
+
+示例 1:
+
+输入: [1, 2, 3]
+	输出 : "onetwothree"
+	解释 :
+	有三个线程会被异步启动。
+	输入[1, 2, 3] 表示线程 A 将会调用 one() 方法，线程 B 将会调用 two() 方法，线程 C 将会调用 three() 方法。
+	正确的输出是 "onetwothree"。
+
+	来源：力扣（LeetCode）
+	链接：https ://leetcode-cn.com/problems/print-in-order
+	著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+
+#include <iostream>
+#include <vector>
+#include <thread>
+
+using namespace std;
+constexpr static int FirstThread = 0x01 << 0;
+constexpr static int SecondThread = 0x01 << 1;
+constexpr static int ThirdThread = 0x01 << 2;
+class Foo {
+public:
+	Foo() {
+		powerBy = FirstThread;
+	}
+
+	void first() {
+		// printFirst() outputs "first". Do not change or remove this line.
+		cout << "printFirst" << endl;
+
+	}
+
+	void second() {
+
+		// printSecond() outputs "second". Do not change or remove this line.
+		cout << "printFirst" << endl;
+	}
+
+	void third() {
+
+		// printThird() outputs "third". Do not change or remove this line.
+		cout << "printFirst" << endl;
+	}
+private:
+	int powerBy;
+};
+
+int main()
+{
+
+	return 0;
+}
---
+++ countPrimes/main.cpp
@@ -0,0 +1,41 @@
+/*
+
+统计所有小于非负整数 n 的质数的数量。
+
+示例:
+
+输入: 10
+输出: 4
+解释: 小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7 。
+
+*/
+#include <iostream>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	int countPrimes(int n) {
+		unordered_set<int> numSet;
+		int i=0;
+		while (i<n)
+		{
+
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+
+		}
+
+
+	}
+};
+
+int main()
+{
+	
+
+}
